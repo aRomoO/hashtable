ab35d7bb4f6fe31a807f09c19d4677e57d631472
frediromo <frediromo@gmail.com>
2023-11-21 10:42:53 -0600

    Initial commit

---
+++ MyHashTable.cpp
@@ -0,0 +1,33 @@
+//
+// Created by fredi on 21/11/23.
+//
+
+#include "MyHashTable.h"
+#include "list"
+
+MyHashTable::MyHashTable(){
+    this->size=0;   //Valores almacenados
+    this->sizeA=11; //Capacidad
+
+    for (int i = 0; i < sizeA; ++i)
+    {
+        this->tabla[i] = new std::list<int>(this->sizeA);
+    }
+}
+int MyHashTable::getPos(std::string key)
+{
+    size_t hashC = std::hash< std::string > {}(key);
+    int hashCode = static_cast<int>(hashC);
+    return 0;
+}
+
+bool MyHashTable::isEmpty()
+{
+    return (sizeof(tabla) == 0);
+}
+
+void MyHashTable::put(std::string key, int value)
+{
+    //Hacer la funcion de hash para encontrar el lugar al que pertenece el elemento
+    //Añadir a la linked list de esa pocición
+}
---
+++ MyHashTable.h
@@ -0,0 +1,33 @@
+//
+// Created by fredi on 21/11/23.
+//
+
+#ifndef HASHTABLE_MYHASHTABLE_H
+#define HASHTABLE_MYHASHTABLE_H
+
+#include "string"
+#include "list"
+
+
+class MyHashTable{
+public:  //Recuerden que esto sólo lo hacemos para poder revisar las actividades pero debería ser público
+    std::list<int> * tabla; //Arreglo dinamico que contiene linkedlists
+
+    int size;         //Cantidad de valores que hay almacenados en la tabla
+    int sizeA;        //Capacidad max de la lista
+
+
+public:
+    void rehashing(); //Crece el arreglo al doble del tamaño del arreglo actual + 1
+    int getPos(std::string key);
+
+    MyHashTable();  //Tamaño inicial 11
+    ~MyHashTable();
+    bool isEmpty(); //O(1)
+    void put(std::string key,int value);
+    int get(std::string key);
+    void remove(std::string key);
+};
+
+
+#endif //HASHTABLE_MYHASHTABLE_H
